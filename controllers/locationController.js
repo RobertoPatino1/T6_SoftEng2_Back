ee575d4cdfec2d93e566d6ac5166a7f364739343
rimacias <105806044+rimacias@users.noreply.github.com>
2024-06-16 01:48:03 -0500

    method get location for locationController

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -15,3 +15,20 @@ exports.updateLocation = async (req, res) => {
 		return res.status(400).json({ error: error.message });
 	}
 };
+
+exports.getLocation = async (req, res) => {
+	const { uid } = req.params;
+
+	try {
+		const location = await db.collection("locations").doc(uid).get();
+
+		if (!location.exists) {
+			return res.status(404).json({ error: "Location not found" });
+		}
+
+		return res.status(200).json(location.data());
+	} catch (error) {
+		return res.status(400).json({ error: error.message });
+	}
+}
+
